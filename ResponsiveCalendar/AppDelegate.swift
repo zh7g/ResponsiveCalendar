993cd4b08ce1aeb1c212b174d86c9583edb7f680
zzp <cstdint@gmail.com>
2015-07-26 16:17:45 +0800

    Initial Commit

---
+++ ResponsiveCalendar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ResponsiveCalendar
+//
+//  Created by zzp on 15/7/26.
+//  Copyright (c) 2015年 zzp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ResponsiveCalendar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ResponsiveCalendar
+//
+//  Created by zzp on 15/7/26.
+//  Copyright (c) 2015年 zzp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
